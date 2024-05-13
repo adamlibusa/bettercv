67e035b9fdae0af1457916838f6d47a2786df332
Adam Libuša <adam.libusa@gmail.com>
2024-05-13 13:54:16 +0200

    Frontend: Initial commit

---
+++ frontend/app/src/pages/CvPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h1>Hello, world!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CvPate',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .container {
+    h1 {
+      background: red;
+    }
+  }
+</style>
