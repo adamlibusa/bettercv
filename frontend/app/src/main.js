85f479a372539e4b2ef50484afd368f325fe1a99
Adam Libuša <adam.libusa@gmail.com>
2024-05-13 15:33:49 +0200

    Added i18n

--- frontend/app/src/main.js
+++ frontend/app/src/main.js
@@ -1,6 +1,25 @@
 import { createApp } from 'vue'
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap.css'
+import i18next from 'i18next'
+import I18NextVue from 'i18next-vue'
+import enTexts from '@/i18n/en.yaml'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+i18next.init({
+  debug: true,
+  lng: 'en',
+  interpolation: {
+    escapeValue: false
+  },
+  fallbackLng: false,
+  resources: {
+    en: {
+      translation: enTexts
+    }
+  }
+})
+
+createApp(App)
+  .use(I18NextVue, {i18next})
+  .mount('#app')
